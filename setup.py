db90af4cba1b6d45ae763352c5db18a36291f92c
Michał Papierski <michal@papierski.net>
2023-05-11 01:21:14 +0200

    Fix CLI runner

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["obligacjeskarbowe = obligacjeskarbowe.__main__:main"]
+        "console_scripts": ["obligacjeskarbowe = obligacjeskarbowe.__main__:cli"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
